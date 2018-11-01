96a669d446a794ce93b1f8d5f5e6a0c4b3df4e86
Emil Albin Ryberg <emil.a.ryberg@gmail.com>
2018-11-01 11:10:32 +0100

    Dynamics..a.a.a.a

--- teensy/teensy/CrustCrawler/ArmDynamics.cpp
+++ teensy/teensy/CrustCrawler/ArmDynamics.cpp
@@ -4,11 +4,13 @@
 
 #include "ArmDynamics.h"
 void ArmDynamics::add_Gravity(const double theta[3], double tau[3]) {
+	double t1Offset = theta[1] - (M_PI / 2);
+	double t2Offset = theta[2] - (M_PI / 2);
 	tau[0] += 0;
-	//tau[1] += g * (l[1] * m[2] * sin(theta[1]) + lc[1] * m[1] * sin(theta[1]) + lc[2] * m[2] * sin(theta[1] + theta[2] - (M_PI / 2)));
-	tau[1] += 4*g*(sin(theta[2])*cos(theta[1])*cos(theta[1])*cos(theta[2]) * lc[1] * m[1] + sin(theta[1]) * (cos(theta[2]) * cos(theta[2]) * lc[1] * m[1] + (lc[0] * m[0]) / 2.0 - (lc[1] * m[1]) / 2.0)*cos(theta[1]) - (sin(theta[2]) * cos(theta[2]) * lc[1] * m[1]) / 2.0);
-	//tau[2] += g * lc[2] * m[2] * sin(theta[1] + theta[2] - (M_PI / 2));
-	tau[2] += 4 * lc[1] * m[1] * g * (cos(theta[1])*cos(theta[1])*cos(theta[2])*sin(theta[2]) + sin(theta[1])*(cos(theta[2])*cos(theta[2]) - 0.5) * cos(theta[1]) - (sin(theta[2]) * cos(theta[2])) / 2.0);
+	tau[1] += g * (l[0] * m[1] * sin(t1Offset) + lc[0] * m[0] * sin(t1Offset) + lc[1] * m[1] * sin(t1Offset + t2Offset));
+	//tau[1] += 4*g*(sin(theta[2])*cos(theta[1])*cos(theta[1])*cos(theta[2]) * lc[1] * m[1] + sin(theta[1]) * (cos(theta[2]) * cos(theta[2]) * lc[1] * m[1] + (lc[0] * m[0]) / 2.0 - (lc[1] * m[1]) / 2.0)*cos(theta[1]) - (sin(theta[2]) * cos(theta[2]) * lc[1] * m[1]) / 2.0);
+	tau[2] += g * lc[1] * m[1] * sin(t1Offset + t2Offset);
+	//tau[2] += 4 * lc[1] * m[1] * g * (cos(theta[1])*cos(theta[1])*cos(theta[2])*sin(theta[2]) + sin(theta[1])*(cos(theta[2])*cos(theta[2]) - 0.5) * cos(theta[1]) - (sin(theta[2]) * cos(theta[2])) / 2.0);
 }
 
 array<double, 3> ArmDynamics::ComputeOutputTorque(array<double, 3> controlAccelerations, array<double, 3> thetaFeedback, array<double, 3> dThetaFeedback)
